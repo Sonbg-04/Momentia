6694955c902a0f9e3ff4765b9e74f2803213bee2
Son <lvson21102k4@gmail.com>
2025-09-12 20:00:43 +0700

    Update backend framework

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/momentia', function () {
+    return view('main');
+});
--- Laravel/vite.config.js
+++ Laravel/vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/src/components/main/main.tsx"
+            ],
             refresh: true,
         }),
+        react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
 });
