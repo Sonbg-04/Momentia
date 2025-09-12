574335491dc9fcc09d6370a9b817345afabd236f
Son <lvson21102k4@gmail.com>
2025-09-12 19:39:17 +0700

    Update

--- Laravel/vite.config.js
+++ Laravel/vite.config.js
@@ -1,25 +1,13 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/src/components/main/main.tsx"
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        react(),
         tailwindcss(),
     ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "resources/js"),
-        },
-    },
 });
