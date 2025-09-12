c7f7cfee3c7649ebe99f783e19868f50c44a7d46
Son <lvson21102k4@gmail.com>
2025-09-12 20:37:08 +0700

    Update web.php

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', function () {
 Route::get('/momentia', function () {
     return view('main');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return 'Admin Users Page';
+    });
+});
