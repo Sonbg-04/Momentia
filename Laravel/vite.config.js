3a234d4120fb983e9fc59242016b29a08fafb81c
Son <lvson21102k4@gmail.com>
2025-09-20 22:25:19 +0700

    Rename file main.tsx
    Main.tsx

--- Laravel/vite.config.js
+++ Laravel/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             input: [
                 "resources/css/app.css",
                 "resources/js/app.js",
-                "resources/src/components/main/main.tsx"
+                "resources/src/components/main/Main.tsx"
             ],
             refresh: true,
         }),
